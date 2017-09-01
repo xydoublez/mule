b91e62205d9558c65131dcfff87cd88e6d953fd7
JuanDesi <juan.desimoni@mulesoft.com>
2017-09-01 11:39:28 -0300

    MULE-13317: support calling operations defined in the same module + more tests

---
+++ modules/extensions-xml-support/src/test/java/org/mule/test/functional/ModuleCallingOperationsWithinModuleWithMultipleGlobalElementsTestCase.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.functional;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
+
+import org.mule.extension.file.common.api.FileAttributes;
+import org.mule.runtime.api.connection.ConnectionProvider;
+import org.mule.runtime.api.connection.ConnectionValidationResult;
+import org.mule.runtime.api.exception.MuleException;
+import org.mule.runtime.api.message.Message;
+import org.mule.runtime.extension.api.runtime.config.ConfigurationInstance;
+import org.mule.tck.junit4.rule.SystemProperty;
+
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class ModuleCallingOperationsWithinModuleWithMultipleGlobalElementsTestCase extends AbstractXmlExtensionMuleArtifactFunctionalTestCase {
+
+  private static final String SUB_DIRECTORY_NAME_A = "subDirectoryA";
+  private static final String SUB_DIRECTORY_NAME_B = "subDirectoryB";
+
+  @ClassRule
+  public static TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+  @Rule
+  public SystemProperty workingDir = new SystemProperty("workingDir", temporaryFolder.getRoot().getAbsolutePath());
+
+  @BeforeClass
+  public static void setUp() throws Exception {
+    if (!temporaryFolder.getRoot().exists()) {
+      temporaryFolder.getRoot().mkdir();
+    }
+    temporaryFolder.newFolder(SUB_DIRECTORY_NAME_A);
+    temporaryFolder.newFolder(SUB_DIRECTORY_NAME_B);
+  }
+
+  @AfterClass
+  public static void tearDown() {
+    temporaryFolder.delete();
+  }
+
+  @Override
+  protected String getModulePath() {
+    return "modules/module-calling-operations-within-module-with-global-elements.xml";
+  }
+
+  @Override
+  protected String getConfigFile() {
+    return "flows/flows-using-module-calling-operations-within-module-with-global-elements.xml";
+  }
+
+  @Test
+  public void testConnectionConfigPatternA() throws Exception {
+    doTestConnection("configPatternA");
+  }
+
+  @Test
+  public void testConnectionConfigPatternB() throws Exception {
+    doTestConnection("configPatternB");
+  }
+
+  private void doTestConnection(String globalElement) throws MuleException {
+    ConfigurationInstance config = muleContext.getExtensionManager().getConfiguration(globalElement, testEvent());
+    assertThat(config, is(notNullValue()));
+    assertThat(config.getConnectionProvider().isPresent(), is(true));
+    final ConnectionProvider connectionProvider = config.getConnectionProvider().get();
+    final Object connect = connectionProvider.connect();
+    final ConnectionValidationResult connectionValidationResult = connectionProvider.validate(connect);
+    assertThat(connectionValidationResult.isValid(), is(true));
+    connectionProvider.disconnect(connect);
+  }
+
+  @Test
+  public void listPatternA() throws Exception {
+    assertFlowResult("list-pattern-a", SUB_DIRECTORY_NAME_A);
+  }
+
+  @Test
+  public void listPatternB() throws Exception {
+    assertFlowResult("list-pattern-b", SUB_DIRECTORY_NAME_B);
+  }
+
+  private void assertFlowResult(String flowName, String subDirectoryName) throws Exception {
+    List<Message> messages = (List<Message>) flowRunner(flowName).run().getMessage().getPayload().getValue();
+    assertThat(messages, is(notNullValue()));
+    assertThat(messages, hasSize(1));
+    FileAttributes attributes = (FileAttributes) messages.get(0).getAttributes().getValue();
+    assertThat(attributes.getName(), is(subDirectoryName));
+  }
+}
