b2d89599ebfb32eacafe2c9304d3394ce7f73a6b
Guillermo Fernandes <guillermo.fernandes@mulesoft.com>
2017-11-10 14:51:49 -0300

    MULE-13958: Remove synchronze

--- core/src/main/java/org/mule/runtime/core/api/registry/SpiServiceRegistry.java
+++ core/src/main/java/org/mule/runtime/core/api/registry/SpiServiceRegistry.java
@@ -21,7 +21,7 @@ import java.util.ServiceLoader;
 public class SpiServiceRegistry extends AbstractServiceRegistry {
 
   @Override
-  protected synchronized <T> Collection<T> doLookupProviders(Class<T> providerClass, ClassLoader classLoader) {
+  protected <T> Collection<T> doLookupProviders(Class<T> providerClass, ClassLoader classLoader) {
     Iterator<T> iterator = ServiceLoader.load(providerClass, classLoader).iterator();
     if (iterator.hasNext()) {
       return copyOf(iterator);
